c08382e9b4a8e6c78b62325524ff65d172371591
12vai <vailetchiwowa@gmail.com>
2023-08-10 14:20:26 -0700

    py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "pythonic"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print(f"First 3 letters: {}". format(word_first_3))
-print(f"Last 2 letters: {}". format(word_last_2))
-print(f"Middle word: {}". format(middle_word))
+print("word_first_3:", word_first_3)
+print("word_last_2:", word_last_2)
+print("middle_word:", middle_word)
 
