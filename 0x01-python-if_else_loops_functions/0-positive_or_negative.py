dcd915b936b0e5707482b397f5a3ac00b2b7566b
12vai <vailetchiwowa@gmail.com>
2023-08-10 16:52:46 -0700

    py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 import random
-
 number = random.randint(-10, 10)
-
 print("The number:", number)
-
 if number > 0:
-    print("is positive")
+    print("{} is positive".hormat(number))
 elif number == 0:
-    print("is zero")
-else:
-    print("is negative")
+    print("{} is zero".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
 
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet .py
-
-"""print the alphabet in lowercase, not followed by a new line,"""
-for char in range(ord('a'), ord('z')+1):
-    print(chr(char), end='')
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
 
