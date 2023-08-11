8b5af195f45fc552e417daa5d65ed86b17c6c770
12vai <vailetchiwowa@gmail.com>
2023-08-11 13:21:29 -0700

    py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+a = 10
+b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-def add(x, y):
-    return x + y
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
 
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
-        return "Cannot divide by zero"
-
+    print("{:d} + {:d} = {:d}".format(a, b, add_result))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
