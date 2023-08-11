8083c95c5ba5b56fd710bfec079cb41d8ae29cb5
12vai <vailetchiwowa@gmail.com>
2023-08-11 12:54:34 -0700

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import builtins
-builtins.print("#pythoniscool")
+setattr(builtins, "__import__", None)
+setattr(builtins, "eval", None)
+setattr(builtins, "open", None)
+setattr(builtins, "print", lambda x: __import__("sys").stdout.write(x + "\n"))
+__import__("sys").stdout.write("#pythoniscool\n")
