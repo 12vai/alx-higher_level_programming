2ead78b4ccad54a7c1d06d18a5ede674360936ca
12vai <vailetchiwowa@gmail.com>
2023-08-11 12:15:57 -0700

    py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,35 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    """print the addition of all arguments."""
+    import sys
 
-import sys
-
-def add_arguments():
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-    return total
-
-def add_user_inputs():
-    total = 0
-    while True:
-        user_input = input("Enter a number (or 'end' to finish): ")
-        if user_input == "end":
-            break
-        total += int(user_input)
-    return total
-
-if __name__ == "__main__":
-    print("Choose an option:")
-    print("1. Add command-line arguments")
-    print("2. Add user inputs")
-    
-    option = input("Enter the option number: ")
-    
-    if option == "1":
-        total = add_arguments()
-    elif option == "2":
-        total = add_user_inputs()
-    else:
-        print("Invalid option")
-        sys.exit(1)
-    
-    print("Total:", total)
+    print("{}".format(total))
