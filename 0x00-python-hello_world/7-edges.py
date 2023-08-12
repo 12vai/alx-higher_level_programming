9abfd47c3d671ac280b1132c7bdd5b238390c5c0
12vai <vailetchiwowa@gmail.com>
2023-08-12 06:07:22 -0700

    py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("word_first_3:", word_first_3)
-print("word_last_2:", word_last_2)
-print("middle_word:", middle_word)
 
-word = word_last_2 + word_first_3
-print("word:", word)
-print("first_letter:", word[0])
-print("last_letter:", word[-1])
+print(f"word_first_3: {word_first_3}")
+print(f"word_last_2: {word_last_2}")
+print(f"middle_word: {middle_word}")
