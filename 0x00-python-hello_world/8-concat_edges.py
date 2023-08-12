eb1d81cfab3a941623f1e3c147782ce813c7344b
12vai <vailetchiwowa@gmail.com>
2023-08-12 06:16:53 -0700

    py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+
 str = "python is an interpreted, interactive, object_oriented programming\
         language that combines remarkable power with very clear syntax"
-        str = str[39:66] + str[106:112] + str[:6]
-        print(str)
+
+str = str[39:66] + str[106:112] + str[:6]
+
+word_first_3 = str[:3]
+word_last_2 = str[-2:]
+middle_word = str[1:-1]
+
+print(f"word_first_3: {word_first_3}")
+print(f"word_last_2: {word_last_2}")
+print(f"middle_word: {middle_word}")
+
+word = word_last_2 + word_first_3
+print(f"word: {word}")
+print(f"first_letter: {word[0]}")
+print(f"last_letter: {word[-1]}")
+
+reversed_word = word[::-1]
+print(f"reversed_word: {reversed_word}")
+print(f"word length: {len(word)}")
