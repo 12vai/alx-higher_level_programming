fdb6b1a0a7d17dc5cc32d67bfd46f558255c59dc
12vai <vailetchiwowa@gmail.com>
2023-08-12 06:24:22 -0700

    npy

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,12 +1,7 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 str = "python is an interpreted, interactive, object_oriented programming\
         language that combines remarkable power with very clear syntax"
         str = str[39:66] + str[106:112] + str[:6]
         print(str)
 
-print("object", end='')
-print("-", end='')
-print("oriented", end='')
-print(" programming", end='')
-print(" with Python")
 
