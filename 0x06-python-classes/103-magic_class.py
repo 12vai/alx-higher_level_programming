e28a2db54c7debbdd3fec8f8238069812f3d9b54
12vai <vailetchiwowa@gmail.com>
2023-08-30 09:53:53 -0700

    py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Module providing class 'MagicClass' to replicate bytecode for Holberton"""
+""" Module providing class 'MagicClass' to replicate bytecode for Holberton """
 import math
 
-class MagicClass:
-    """
-    Definition of a class to replicate the aforementioned bytecode
-    """
+
+class MagicClass():
+    """ Definition of a class to replicate the afformentioned bytecode """
     def __init__(self, radius=0):
-        """
-        Instantiate a MagicClass object to represent a circle
-        """
-        self.__radius = 0  # Initialize radius to 0
-
-        # Check if radius is a valid number (int or float)
-        if not isinstance(radius, (int, float)):
+        """ Instantiate a MagicClass object to represent a circle """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-
-        self.__radius = radius  # Assign the provided radius
+        self.__radius = radius
 
     def area(self):
-        """
-        Compute the area of a circle
-        """
+        """ Compute the area of a circle """
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """
-        Compute the circumference of a circle
-        """
+        """ Compute the circumference of a circle """
         return 2 * math.pi * self.__radius
