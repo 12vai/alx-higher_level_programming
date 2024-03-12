04a5c29192381330125dc643c5c433c0ac1452cb
12vai <vailetchiwowa@gmail.com>
2024-03-12 12:50:27 -0700

    python

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+import dis
+
+# Define the bytecode
+bytecode = b'\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00S'
+
+# Disassemble the bytecode
+dis.dis(bytecode)
+
+def magic_calculation(a, b, c):
+    # Write your Python function here based on the analysis of the disassembled bytecode
+    pass
+
